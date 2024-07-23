f296e8f2405ee055aead774f7f4a6cd851e0e99a
Andrzej Liszka <andrzej.liszka@gmail.com>
2024-07-23 16:21:30 +0200

    wip 2

--- adapters/repos/db/roaringsetrange/segment_reader.go
+++ adapters/repos/db/roaringsetrange/segment_reader.go
@@ -14,7 +14,9 @@ package roaringsetrange
 import (
 	"context"
 	"fmt"
+	"math"
 
+	"github.com/weaviate/sroar"
 	"github.com/weaviate/weaviate/adapters/repos/db/roaringset"
 	"github.com/weaviate/weaviate/entities/filters"
 )
@@ -34,53 +36,280 @@ func (r *SegmentReader) Read(ctx context.Context, value uint64, operator filters
 	}
 
 	switch operator {
-	// case filters.OperatorEqual:
-	// case filters.OperatorNotEqual:
-	// case filters.OperatorLessThanEqual:
-	// case filters.OperatorLessThan:
-	// case filters.OperatorGreaterThan:
+	case filters.OperatorEqual:
+		return r.readEqual(ctx, value)
+
+	case filters.OperatorNotEqual:
+		return r.readNotEqual(ctx, value)
+
+	case filters.OperatorLessThan:
+		return r.readLessThan(ctx, value)
+
+	case filters.OperatorLessThanEqual:
+		return r.readLessThanEqual(ctx, value)
+
+	case filters.OperatorGreaterThan:
+		return r.readGreaterThan(ctx, value)
+
 	case filters.OperatorGreaterThanEqual:
 		return r.readGreaterThanEqual(ctx, value)
+
 	default:
 		return roaringset.BitmapLayer{}, fmt.Errorf("operator %v not supported for segments of strategy %q",
 			operator.Name(), "roaringsetrange") // TODO move strategies to separate package?
 	}
 }
 
+func (r *SegmentReader) firstLayer() (roaringset.BitmapLayer, bool) {
+	_, layer, ok := r.cursor.First()
+	if !ok {
+		return roaringset.BitmapLayer{
+			Additions: sroar.NewBitmap(),
+			Deletions: sroar.NewBitmap(),
+		}, false
+	}
+
+	var deletions *sroar.Bitmap
+	if layer.Deletions == nil {
+		deletions = sroar.NewBitmap()
+	} else {
+		deletions = layer.Deletions.Clone()
+	}
+
+	if layer.Additions.IsEmpty() {
+		return roaringset.BitmapLayer{
+			Additions: sroar.NewBitmap(),
+			Deletions: deletions,
+		}, false
+	}
+	// additions will be cloned when needed by merging methods
+	return roaringset.BitmapLayer{
+		Additions: layer.Additions,
+		Deletions: deletions,
+	}, true
+}
+
+func (r *SegmentReader) readEqual(ctx context.Context, value uint64,
+) (roaringset.BitmapLayer, error) {
+	if value == 0 {
+		return r.readLessThanEqual(ctx, value)
+	}
+	if value == math.MaxUint64 {
+		return r.readGreaterThanEqual(ctx, value)
+	}
+
+	firstLayer, ok := r.firstLayer()
+	if !ok {
+		return firstLayer, nil
+	}
+
+	eq, err := r.mergeBetween(ctx, value, value+1, firstLayer.Additions)
+	if err != nil {
+		return roaringset.BitmapLayer{}, err
+	}
+
+	return roaringset.BitmapLayer{
+		Additions: eq,
+		Deletions: firstLayer.Deletions,
+	}, nil
+}
+
+func (r *SegmentReader) readNotEqual(ctx context.Context, value uint64,
+) (roaringset.BitmapLayer, error) {
+	if value == 0 {
+		return r.readGreaterThan(ctx, value)
+	}
+	if value == math.MaxUint64 {
+		return r.readLessThan(ctx, value)
+	}
+
+	firstLayer, ok := r.firstLayer()
+	if !ok {
+		return firstLayer, nil
+	}
+
+	neq := firstLayer.Additions.Clone()
+	eq, err := r.mergeBetween(ctx, value, value+1, firstLayer.Additions)
+	if err != nil {
+		return roaringset.BitmapLayer{}, err
+	}
+
+	neq.AndNot(eq)
+	return roaringset.BitmapLayer{
+		Additions: neq,
+		Deletions: firstLayer.Deletions,
+	}, nil
+}
+
+func (r *SegmentReader) readLessThan(ctx context.Context, value uint64,
+) (roaringset.BitmapLayer, error) {
+	firstLayer, ok := r.firstLayer()
+	if !ok {
+		return firstLayer, nil
+	}
+
+	if value == 0 {
+		// no value is < 0
+		return roaringset.BitmapLayer{
+			Additions: sroar.NewBitmap(),
+			Deletions: firstLayer.Deletions,
+		}, nil
+	}
+
+	lt := firstLayer.Additions.Clone()
+	gte, err := r.mergeGreaterThanEqual(ctx, value, firstLayer.Additions)
+	if err != nil {
+		return roaringset.BitmapLayer{}, err
+	}
+
+	lt.AndNot(gte)
+	return roaringset.BitmapLayer{
+		Additions: lt,
+		Deletions: firstLayer.Deletions,
+	}, nil
+}
+
+func (r *SegmentReader) readLessThanEqual(ctx context.Context, value uint64,
+) (roaringset.BitmapLayer, error) {
+	firstLayer, ok := r.firstLayer()
+	if !ok {
+		return firstLayer, nil
+	}
+
+	if value == math.MaxUint64 {
+		// all values are <= max uint64
+		return firstLayer, nil
+	}
+
+	lte := firstLayer.Additions.Clone()
+	gte1, err := r.mergeGreaterThanEqual(ctx, value+1, firstLayer.Additions)
+	if err != nil {
+		return roaringset.BitmapLayer{}, err
+	}
+
+	lte.AndNot(gte1)
+	return roaringset.BitmapLayer{
+		Additions: lte,
+		Deletions: firstLayer.Deletions,
+	}, nil
+}
+
+func (r *SegmentReader) readGreaterThan(ctx context.Context, value uint64,
+) (roaringset.BitmapLayer, error) {
+	firstLayer, ok := r.firstLayer()
+	if !ok {
+		return firstLayer, nil
+	}
+
+	if value == math.MaxUint64 {
+		// no value is > max uint64
+		return roaringset.BitmapLayer{
+			Additions: sroar.NewBitmap(),
+			Deletions: firstLayer.Deletions,
+		}, nil
+	}
+
+	gte1, err := r.mergeGreaterThanEqual(ctx, value+1, firstLayer.Additions)
+	if err != nil {
+		return roaringset.BitmapLayer{}, err
+	}
+
+	return roaringset.BitmapLayer{
+		Additions: gte1,
+		Deletions: firstLayer.Deletions,
+	}, nil
+}
+
 func (r *SegmentReader) readGreaterThanEqual(ctx context.Context, value uint64,
 ) (roaringset.BitmapLayer, error) {
-	bit, layer, ok := r.cursor.First()
+	firstLayer, ok := r.firstLayer()
 	if !ok {
-		return roaringset.BitmapLayer{}, nil
-	}
-	if layer.Additions.IsEmpty() {
-		return layer, nil
+		return firstLayer, nil
 	}
 
+	gte, err := r.mergeGreaterThanEqual(ctx, value, firstLayer.Additions)
+	if err != nil {
+		return roaringset.BitmapLayer{}, err
+	}
+
+	return roaringset.BitmapLayer{
+		Additions: gte,
+		Deletions: firstLayer.Deletions,
+	}, nil
+}
+
+func (r *SegmentReader) mergeGreaterThanEqual(ctx context.Context, value uint64,
+	all *sroar.Bitmap,
+) (*sroar.Bitmap, error) {
 	ANDed := false
-	resBM := layer.Additions
-	delBM := layer.Deletions
+	result := all
 
-	for bit, layer, ok = r.cursor.Next(); ok; bit, layer, ok = r.cursor.Next() {
+	for bit, layer, ok := r.cursor.Next(); ok; bit, layer, ok = r.cursor.Next() {
 		if ctx.Err() != nil {
-			return roaringset.BitmapLayer{}, ctx.Err()
+			return nil, ctx.Err()
 		}
 
 		if value&(1<<(bit-1)) != 0 {
 			if !ANDed {
-				resBM = resBM.Clone()
+				result = result.Clone()
 			}
 			ANDed = true
-			resBM.And(layer.Additions)
-			resBM = roaringset.Condense(resBM)
+			result.And(layer.Additions)
+			result = roaringset.Condense(result)
 		} else if ANDed {
-			resBM.Or(layer.Additions)
+			result.Or(layer.Additions)
 		}
 	}
 
 	if !ANDed {
-		resBM = resBM.Clone()
+		result = result.Clone()
 	}
 
-	return roaringset.BitmapLayer{Additions: resBM, Deletions: delBM}, nil
+	return result, nil
+}
+
+func (r *SegmentReader) mergeBetween(ctx context.Context, valueMinInc, valueMaxExc uint64,
+	all *sroar.Bitmap,
+) (*sroar.Bitmap, error) {
+	ANDedMin := false
+	ANDedMax := false
+	resultMin := all
+	resultMax := all
+
+	for bit, layer, ok := r.cursor.Next(); ok; bit, layer, ok = r.cursor.Next() {
+		if ctx.Err() != nil {
+			return nil, ctx.Err()
+		}
+
+		var b uint64 = 1 << (bit - 1)
+
+		if valueMinInc&b != 0 {
+			if !ANDedMin {
+				resultMin = resultMin.Clone()
+			}
+			ANDedMin = true
+			resultMin.And(layer.Additions)
+			resultMin = roaringset.Condense(resultMin)
+		} else if ANDedMin {
+			resultMin.Or(layer.Additions)
+		}
+
+		if valueMaxExc&b != 0 {
+			if !ANDedMax {
+				resultMax = resultMax.Clone()
+			}
+			ANDedMax = true
+			resultMax.And(layer.Additions)
+			resultMax = roaringset.Condense(resultMax)
+		} else if ANDedMax {
+			resultMax.Or(layer.Additions)
+		}
+	}
+
+	if !ANDedMin {
+		resultMin = resultMin.Clone()
+	}
+	resultMin.AndNot(resultMax)
+
+	return resultMin, nil
 }
